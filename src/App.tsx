7d8b108462037e4f206a94d5384d4ac677e7b69b
DanielPizarro22 <pizarrodaniel17@gmail.com>
2025-10-26 10:49:20 -0300

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route, NavLink , Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
@@ -10,35 +12,33 @@ import CreatePost from "./pages/CreatePost";
 function App() {
   return (
     <BrowserRouter>
-      <nav className="navbar">
-        <NavLink to="/" className="navlink">
-          Home
-        </NavLink>
-        <NavLink to="/create-post" className="navlink">
-          Create Post
-        </NavLink>
-        <NavLink to="/login" className="navlink">
-          Login
-        </NavLink>
-        <NavLink to="/post/1" className="navlink">
-          Post Detail
-        </NavLink>
-        <NavLink to="/profile" className="navlink">
-          Profile
-        </NavLink>
-        <NavLink to="/register" className="navlink">
-          Register
-        </NavLink>
-      </nav>
+      <Navbar bg="light" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand as={NavLink} to="/">MyBlog</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/create-post">Create Post</Nav.Link>
+              <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
+              <Nav.Link as={NavLink} to="/post/1">Post Detail</Nav.Link>
+              <Nav.Link as={NavLink} to="/profile">Profile</Nav.Link>
+              <Nav.Link as={NavLink} to="/register">Register</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-post" element={<CreatePost />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/post/:id" element={<PostDetail />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Container style={{ paddingTop: '80px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/post/:id" element={<PostDetail />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
